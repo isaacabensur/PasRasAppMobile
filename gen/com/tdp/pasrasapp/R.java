5ac2050b25d75dfc639b91ee0a23ec6cb33cc2a4
Alumnos <Alumnos@F32-29.upc.edu.pe>
2014-11-08 14:40:03 -0500

    pa la gentis

--- gen/com/tdp/pasrasapp/R.java
+++ gen/com/tdp/pasrasapp/R.java
@@ -29,6 +29,7 @@ public final class R {
     }
     public static final class layout {
         public static final int actividad_principal=0x7f030000;
+        public static final int prlogueo=0x7f030001;
     }
     public static final class menu {
         public static final int actividad_principal=0x7f070000;
