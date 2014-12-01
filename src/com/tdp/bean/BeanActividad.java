85a12996b677d9e2e60031d6c0c68d1d8600aa5c
maxromeroupc <MRRI@MRRI-PC>
2014-12-01 00:27:17 -0500

    Bean Actividad

---
+++ src/com/tdp/bean/BeanActividad.java
@@ -0,0 +1,165 @@
+package com.tdp.bean;
+
+import java.util.Date;
+
+public class BeanActividad {
+
+	int id_actividad;
+	int id_planificacion;
+	String descripcion_actividad;
+	int id_tipo_actividad;
+	  int id_ipr;
+	 Date fecha_semana_actividad;
+	 int id_proyecto;
+	  int hh_planificada_lunes;
+	  int hh_real_lunes;
+	  int hh_planificada_martes;
+	  int hh_real_martes;
+	  int hh_planificada_miercoles;
+	  public int getId_actividad() {
+		return id_actividad;
+	}
+	public void setId_actividad(int id_actividad) {
+		this.id_actividad = id_actividad;
+	}
+	public int getId_planificacion() {
+		return id_planificacion;
+	}
+	public void setId_planificacion(int id_planificacion) {
+		this.id_planificacion = id_planificacion;
+	}
+	public String getDescripcion_actividad() {
+		return descripcion_actividad;
+	}
+	public void setDescripcion_actividad(String descripcion_actividad) {
+		this.descripcion_actividad = descripcion_actividad;
+	}
+	public int getId_tipo_actividad() {
+		return id_tipo_actividad;
+	}
+	public void setId_tipo_actividad(int id_tipo_actividad) {
+		this.id_tipo_actividad = id_tipo_actividad;
+	}
+	public int getId_ipr() {
+		return id_ipr;
+	}
+	public void setId_ipr(int id_ipr) {
+		this.id_ipr = id_ipr;
+	}
+	public Date getFecha_semana_actividad() {
+		return fecha_semana_actividad;
+	}
+	public void setFecha_semana_actividad(Date fecha_semana_actividad) {
+		this.fecha_semana_actividad = fecha_semana_actividad;
+	}
+	public int getId_proyecto() {
+		return id_proyecto;
+	}
+	public void setId_proyecto(int id_proyecto) {
+		this.id_proyecto = id_proyecto;
+	}
+	public int getHh_planificada_lunes() {
+		return hh_planificada_lunes;
+	}
+	public void setHh_planificada_lunes(int hh_planificada_lunes) {
+		this.hh_planificada_lunes = hh_planificada_lunes;
+	}
+	public int getHh_real_lunes() {
+		return hh_real_lunes;
+	}
+	public void setHh_real_lunes(int hh_real_lunes) {
+		this.hh_real_lunes = hh_real_lunes;
+	}
+	public int getHh_planificada_martes() {
+		return hh_planificada_martes;
+	}
+	public void setHh_planificada_martes(int hh_planificada_martes) {
+		this.hh_planificada_martes = hh_planificada_martes;
+	}
+	public int getHh_real_martes() {
+		return hh_real_martes;
+	}
+	public void setHh_real_martes(int hh_real_martes) {
+		this.hh_real_martes = hh_real_martes;
+	}
+	public int getHh_planificada_miercoles() {
+		return hh_planificada_miercoles;
+	}
+	public void setHh_planificada_miercoles(int hh_planificada_miercoles) {
+		this.hh_planificada_miercoles = hh_planificada_miercoles;
+	}
+	public int getHh_real_miercoles() {
+		return hh_real_miercoles;
+	}
+	public void setHh_real_miercoles(int hh_real_miercoles) {
+		this.hh_real_miercoles = hh_real_miercoles;
+	}
+	public int getHh_planificada_jueves() {
+		return hh_planificada_jueves;
+	}
+	public void setHh_planificada_jueves(int hh_planificada_jueves) {
+		this.hh_planificada_jueves = hh_planificada_jueves;
+	}
+	public int getHh_real_jueves() {
+		return hh_real_jueves;
+	}
+	public void setHh_real_jueves(int hh_real_jueves) {
+		this.hh_real_jueves = hh_real_jueves;
+	}
+	public int getHh_planificada_viernes() {
+		return hh_planificada_viernes;
+	}
+	public void setHh_planificada_viernes(int hh_planificada_viernes) {
+		this.hh_planificada_viernes = hh_planificada_viernes;
+	}
+	public int getHh_real_viernes() {
+		return hh_real_viernes;
+	}
+	public void setHh_real_viernes(int hh_real_viernes) {
+		this.hh_real_viernes = hh_real_viernes;
+	}
+	public int getHh_planificada_sabado() {
+		return hh_planificada_sabado;
+	}
+	public void setHh_planificada_sabado(int hh_planificada_sabado) {
+		this.hh_planificada_sabado = hh_planificada_sabado;
+	}
+	public int getHh_real_sabado() {
+		return hh_real_sabado;
+	}
+	public void setHh_real_sabado(int hh_real_sabado) {
+		this.hh_real_sabado = hh_real_sabado;
+	}
+	public int getHh_planificada_domingo() {
+		return hh_planificada_domingo;
+	}
+	public void setHh_planificada_domingo(int hh_planificada_domingo) {
+		this.hh_planificada_domingo = hh_planificada_domingo;
+	}
+	public int getHh_real_domingo() {
+		return hh_real_domingo;
+	}
+	public void setHh_real_domingo(int hh_real_domingo) {
+		this.hh_real_domingo = hh_real_domingo;
+	}
+	public int getId_origen() {
+		return id_origen;
+	}
+	public void setId_origen(int id_origen) {
+		this.id_origen = id_origen;
+	}
+	int hh_real_miercoles;
+	  int hh_planificada_jueves;
+	  int hh_real_jueves;
+	  int hh_planificada_viernes;
+	  int hh_real_viernes;
+	  int hh_planificada_sabado;
+	  int hh_real_sabado;
+	  int hh_planificada_domingo;
+	  int hh_real_domingo;
+	  int id_origen;
+	  
+	  
+	
+	
+}
